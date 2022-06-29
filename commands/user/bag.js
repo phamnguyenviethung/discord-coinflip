58b1fbf373115d5ba2a65ab5b65578fb84c4ff3e
Viet Hung <viethung01234@gmail.com>
2022-06-29 12:54:34 +0700

    bag fixed

--- commands/user/bag.js
+++ commands/user/bag.js
@@ -10,9 +10,8 @@ module.exports = {
     try {
       const user = await User.findOne({ id: interaction.user.id });
       if (!user) return interaction.reply("Bạn chưa đăng ký");
-      console.log(user.inventory.wire);
       return interaction.reply(
-        `Plastic: **${user.inventory.plastic}**\nIron: **${user.inventory.iron}**\nTape: **${user.inventory.tape}**\nWire: **${user.inventory.wire}**\nCloth: **${user.inventory.cloth}**`
+        `Plastic: **${user.inventory.plastic}**\nIron: **${user.inventory.iron}**\nTape: **${user.inventory.tape}**\nWire: **${user.inventory.wire}**\nCloth: **${user.inventory.cloth}**\nKnife: **${user.inventory.knife}**`
       );
     } catch (error) {
       console.log(error);
