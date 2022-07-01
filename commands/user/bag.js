4aa921e015c424845533c2664943fc25de71d5ba
Viet Hung <viethung01234@gmail.com>
2022-07-01 13:13:29 +0700

    sting add

--- commands/user/bag.js
+++ commands/user/bag.js
@@ -11,7 +11,7 @@ module.exports = {
       const user = await User.findOne({ id: interaction.user.id });
       if (!user) return interaction.reply("Bạn chưa đăng ký");
       return interaction.reply(
-        `Plastic: **${user.inventory.plastic}**\nIron: **${user.inventory.iron}**\nTape: **${user.inventory.tape}**\nWire: **${user.inventory.wire}**\nCloth: **${user.inventory.cloth}**\nKnife: **${user.inventory.knife}**`
+        `Plastic: **${user.inventory.plastic}**\nIron: **${user.inventory.iron}**\nTape: **${user.inventory.tape}**\nWire: **${user.inventory.wire}**\nCloth: **${user.inventory.cloth}**\nKnife: **${user.inventory.knife}**\nSting: **${user.inventory.sting}**`
       );
     } catch (error) {
       console.log(error);
