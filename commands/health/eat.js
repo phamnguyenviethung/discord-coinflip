1c02eebe145682f1faaf34705df9cfd57d0ffa86
Viet Hung <viethung01234@gmail.com>
2022-07-12 14:07:24 +0700

    eat

--- commands/health/eat.js
+++ commands/health/eat.js
@@ -1,48 +1,21 @@
-const { category } = require("../../utils/category");
-const choices = [];
-category.eat.forEach((item) => {
-  choices.push({
-    name: item,
-    value: item,
-  });
-});
 module.exports = {
   name: "eat",
   description: "Ăn gì đó đê!!!",
   type: "CHAT_INPUT",
-  cooldown: 7,
-  options: [
-    {
-      name: "food",
-      description: "Chọn món",
-      required: true,
-      type: "STRING",
-      choices,
-    },
-  ],
-  run: async (client, interaction, user) => {
-    const food = interaction.options.get("food").value;
+  cooldown: 30,
 
-    const values = {
-      bread: 10,
-      rice: 30,
-      noodle: 50,
-    };
+  run: async (client, interaction, user) => {
     try {
       if (user.health.eat >= 350) {
         return interaction.reply(` Bạn không đói.`);
       }
 
-      if (user.inventory.food[food] <= 0) {
-        return interaction.reply(`Bạn không có để ăn`);
-      }
-
-      user.inventory.food[food] -= 1;
-      user.health.eat += values[food];
+      user.money -= 2000;
+      user.health.eat += 70;
       user.save();
 
       return interaction.reply(
-        `**${interaction.user.username}** vừa bú **${food}** 🍖`
+        `**${interaction.user.username}** vừa bú **1 tô mì giá 2000$** 🍖`
       );
     } catch (error) {
       console.log(error);
