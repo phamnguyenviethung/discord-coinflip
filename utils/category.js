96f490ad23930c06d674203570ac342ab2202a39
Viet Hung <viethung01234@gmail.com>
2022-07-13 15:00:51 +0700

    fix shotgun

--- utils/category.js
+++ utils/category.js
@@ -1,8 +1,8 @@
 module.exports.category = {
   fishing: ["carp", "perch", "phattom", "shark"],
   hunting: ["rabbit", "tiger", "rhino"],
-  metal: ["iron", "plastic", "cloth", "wire", "tape", "shovel", "maycay"],
-  weapon: ["knife", "taser"],
+  metal: ["iron", "plastic", "cloth", "wire", "tape"],
+  weapon: ["knife", "taser", "shotgun"],
   tool: ["huntingrifle", "fishingrod", "shovel", "gas", "maycay", "mask"],
   smuggle: ["tiger", "rhino", "rabbit"],
 };
