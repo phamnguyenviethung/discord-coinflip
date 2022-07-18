a6be2bde33204022998e0bda5715f5c6a8ca4b30
Viet Hung <viethung01234@gmail.com>
2022-07-18 14:43:07 +0700

    fix bank

--- commands/money/withdraw.js
+++ commands/money/withdraw.js
@@ -23,7 +23,7 @@ module.exports = {
       if (user.atm < value || user.atm <= 0) {
         return interaction.reply("Bạn không đủ tiền");
       }
-      const percentage = value - (value * 85) / 100;
+      const percentage = value - (value * 99) / 100;
 
       user.atm -= value;
       user.money += value - percentage;
