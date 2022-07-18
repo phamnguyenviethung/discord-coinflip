cc7b0a573edd9019fb14618c56237acb5f5fd0e6
Viet Hung <viethung01234@gmail.com>
2022-07-18 14:28:46 +0700

    fix salary

--- commands/task/job/dig.js
+++ commands/task/job/dig.js
@@ -6,6 +6,7 @@ module.exports = async (client, interaction, data) => {
   try {
     const { user } = data;
     const { eat, drink, salary, randomQuantity, percent, exp, level } = job.dig;
+    const basicSalary = 650;
 
     const randomItem = random(percent);
 
@@ -18,7 +19,7 @@ module.exports = async (client, interaction, data) => {
     user.health.eat -= eat;
     user.health.drink -= drink;
     user.inventory.metal[randomItem] += randomQuantity;
-    user.money += salary;
+    user.money += _.random(basicSalary + 150, basicSalary + 250) * 1.8;
     user.profile.exp.amount +=
       user.profile.exp.amount < exp && user.profile.exp.level === level ? 1 : 0;
     user.save();
--- commands/task/job/janitor.js
+++ commands/task/job/janitor.js
@@ -4,12 +4,14 @@ const { job } = require("../../../configs/jobConfig");
 
 module.exports = async (client, interaction, data) => {
   try {
+    const basicSalary = 650;
+
     const { user } = data;
     const { salary, eat, drink, exp, level } = job.janitor;
     const randomQuantity = _.random(1, 3);
     user.health.eat -= eat;
     user.health.drink -= drink;
-    user.money += salary;
+    user.money += _.random(basicSalary, basicSalary + 100) * 1.5;
     user.profile.exp.amount +=
       user.profile.exp.amount < exp && user.profile.exp.level === level ? 1 : 0;
     user.save();
